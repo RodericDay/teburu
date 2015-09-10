6093a226a06e1088d5e617b9c22cbe5a4973dc1d
Roderic Day <roderic.day@gmail.com>
2015-09-10 11:22:56 -0400

    Update to game generators

--- static/games/generate_games.py
+++ static/games/generate_games.py
@@ -9,14 +9,12 @@ with open('hanabi.json', 'w') as fp:
 
     pieces+= [("scorearea", 0, 0, 0, "zone", "Score")]
 
-    i = 0
     suits = ["red", "green", "blue", "yellow", "white"]
     values = "1112233445"
     combos = list(itertools.product(suits, values))
     random.shuffle(combos)
-    for suit, value in combos:
+    for i, (suit, value) in enumerate(combos):
         pieces+= [("c"+str(i), 215, 10+2*i, i, "draggable card "+suit, value)]
-        i += 1
 
     for i in range(8):
         pieces+= [("bt"+str(i), 265, 10+2*i, i, "draggable token blue", "")]
@@ -25,13 +23,3 @@ with open('hanabi.json', 'w') as fp:
         pieces+= [("rt"+str(i), 300, 10+2*i, i, "draggable token red", "")]
 
     json.dump({"moves": pieces}, fp)
-# {
-#     "moves": [
-#         ["z1", 0, 0, 0, "zone blue", "Player 1"],
-#         ["z2", 0, 0, 0, "zone green", "Player 2"],
-#         ["scorearea", 0, 0, 0, "zone", "Score"],
-#         ["c01", 250, 10, 1, "draggable yellow card", "1"],
-#         ["c02", 250, 10, 2, "draggable white card", "2"],
-#         ["t01", 250, 100, 3, "draggable red token", ""]
-#     ]
-# }
